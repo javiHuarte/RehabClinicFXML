460193d9fd5c143d19d10351b46a1fdb333aca7c
Usuario <Usuario@LAPTOP-OFTA10EK.lan>
2020-04-26 17:12:04 +0200

    j

--- src/application/MedicalProfessionalViewController.java
+++ src/application/MedicalProfessionalViewController.java
@@ -1,7 +1,8 @@
 package application;
 
 import java.net.URL;
-import java.sql.Date;
+
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -9,27 +10,32 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.cell.PropertyValueFactory;
 import pojos.MedicalProfessional;
 
 
+
+
 public class MedicalProfessionalViewController implements Initializable {
 
+
+	private ObservableList<MedicalProfessional> medicalProfessionals = FXCollections.observableArrayList();
 	//We open access to the DataBase
 	private DBConnection dbConnection = new DBConnection();
 
-	@FXML  private TableView<MedicalProfessional> medicalProfessionaTable = null;
+	@FXML  private TableView<MedicalProfessional> medicalProfessionalTable = null;
 
 	@FXML private TableColumn<MedicalProfessional, Integer> idColumn;
 	@FXML private TableColumn<MedicalProfessional, String> nameColumn;
-	@FXML private TableColumn<MedicalProfessional, Date> dobColumn;
+	@FXML private TableColumn<MedicalProfessional, LocalDate> dobColumn;
 	@FXML private TableColumn<MedicalProfessional, String> sexColumn;
 	@FXML private TableColumn<MedicalProfessional, String> specialtyColumn;
 	@FXML private TableColumn<MedicalProfessional, Integer> phoneColumn;
 	@FXML private TableColumn<MedicalProfessional, String> adressColumn;
 	@FXML private TableColumn<MedicalProfessional, String> emailColumn;
-	@FXML private TableColumn<MedicalProfessional, String> contractColumn;
-	@FXML private TableColumn<MedicalProfessional, Integer> departmentColumn;
+	@FXML private TableColumn<MedicalProfessional, Integer> contractColumn;
+	@FXML private TableColumn<MedicalProfessional, String> departmentColumn;
 	@FXML private TableColumn<MedicalProfessional, String> nifColumn;
 
 	@FXML private Button backButton;
@@ -41,24 +47,42 @@ public class MedicalProfessionalViewController implements Initializable {
 		//configure the table columns
 		idColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, Integer>("id"));
 		nameColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, String>("name"));
-		dobColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, Date>("dob"));
+		dobColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, LocalDate>("dob"));
 		sexColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, String>("sex"));
 		specialtyColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, String>("profession"));
 		phoneColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, Integer>("phoneNumber"));
 		adressColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, String>("adress"));
 		emailColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, String>("email"));
-		contractColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, String>("employee_contractId"));
-		departmentColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, Integer>("dep_id"));
-		nifColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, String>("dep_id"));
+		//contractColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, Integer>("employee_contractId"));
+	//	departmentColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, String>("department"));
+		//nifColumn.setCellValueFactory(new PropertyValueFactory<MedicalProfessional, String>("nif"));
+
+
+	medicalProfessionalTable.setItems(loadMedicalProfessional());
 
 	}
 
 	//Method to load/ list the medical professionals into the the TableView
-	public void listMediacalProfessionals(){
+	/*public void listMediacalProfessionals(){
 
 		ObservableList<MedicalProfessional> medicalProfessionals = FXCollections.observableArrayList();
 		medicalProfessionals.addAll(dbConnection.listAllMedicalProfessionals());
 
 		medicalProfessionaTable.getItems().addAll(medicalProfessionals);
-	}
+
+		this.medicalProfessionals.add(medicalProfessional);
+	}*/
+
+	public ObservableList<MedicalProfessional> loadMedicalProfessional(){
+
+		MedicalProfessional mp = new MedicalProfessional(1, "marta", LocalDate.now(), "female", "surgeon", "email", "calle", 5667778, "555555T", 1, "psicology");
+		this.medicalProfessionals.add(mp);
+
+		System.out.print(medicalProfessionals);
+
+		return medicalProfessionals;
+
+}
+
+
 }
